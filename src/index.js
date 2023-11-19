0c9401bf0c54892d9bb4d76cbb3ee4137c3f81e3
youngboypimpin <rjtekdom@gmail.com>
2023-11-19 20:42:31 +0100

    fixing bugs

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import your main App component or the component you want to render
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
