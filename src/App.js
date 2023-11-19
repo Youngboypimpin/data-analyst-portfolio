4765544fb96c7bab597d95cef61947f21822c18f
youngboypimpin <rjtekdom@gmail.com>
2023-11-19 20:55:50 +0100

    all bugs resolved. it should load properly now

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
